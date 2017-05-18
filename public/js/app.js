e9f1535857f2e657174cf03a56cbcc189ced3df7
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-05-18 15:58:06 -0400

    Making progress w/ the login modal

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,12 @@ console.log('app.js');
 var app = angular.module('EventApp',[]);
 
 app.controller('MainController', ['$http', function($http){
-
-
+   this.toggleRegionA = false;
+   this.showLoginModal = true;
+   // $http({
+   //    method:"GET",
+   //    url: 'http://localhost:3000/users'
+   // }).then(function(response){
+   //    console.log(response);
+   // });
 }]);
