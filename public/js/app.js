eb7b73b40aa3ce34abc72fbe9cd1b175f22841d7
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-05-18 17:53:12 -0400

    all region button works

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,20 @@ var app = angular.module('EventApp',[]);
 
 app.controller('MainController', ['$http', function($http){
    this.toggleRegionA = false;
-   this.showLoginModal = true;
+   this.showLoginModal = false;
+   this.loginForm = true;
+   this.regForm = false;
+   var controller = this;
+   this.showLoginForm = function(){
+      console.log('login form');
+      this.loginForm = true;
+      this.regForm = false;
+   };
+   this.showRegForm = function(){
+      console.log('Registration form');
+      this.loginForm = false;
+      this.regForm = true;
+   };
    // $http({
    //    method:"GET",
    //    url: 'http://localhost:3000/users'
