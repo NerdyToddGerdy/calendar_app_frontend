5acd742ee39d2ae3836d49ba461fdff9783b5741
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-05-18 23:07:02 -0400

    At a stopping point for the night.

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,20 @@
 console.log('app.js');
 
 var app = angular.module('EventApp',[]);
+this.events=[];
 
 app.controller('MainController', ['$http', function($http){
    this.toggleRegionA = false;
+   this.openRegionA = function(){
+      this.toggleRegionA = !this.toggleRegionA;
+      $http({
+         method:"GET",
+         url: 'http://localhost:3000/events'
+      }).then(function(response){
+         console.log(response.data);
+         this.events = response.data;
+      }.bind(this));
+   };
    this.showLoginModal = false;
    this.loginForm = true;
    this.regForm = false;
@@ -18,10 +29,6 @@ app.controller('MainController', ['$http', function($http){
       this.loginForm = false;
       this.regForm = true;
    };
-   // $http({
-   //    method:"GET",
-   //    url: 'http://localhost:3000/users'
-   // }).then(function(response){
-   //    console.log(response);
-   // });
+
+
 }]);
